96a09c46abef1d0c5f782bd4115b7244e14d6dc7
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2024-07-24 12:43:34 +0200

    Add other use cases

--- tests/testing.sh
+++ tests/testing.sh
@@ -4,3 +4,12 @@ function assert() {
       exit 1
     fi
 }
+
+function assert_fail() {
+    if "$@"; then
+        echo "Test failed: command succeeded, but it was expected to fail"
+        exit 1
+    else
+        echo "Test passed: command failed as expected"
+    fi
+}
