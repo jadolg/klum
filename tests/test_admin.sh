1f7d7dc2914cfa760be73137c5d0794c5ec2ce94
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2024-07-24 12:09:40 +0200

    Add test for base case (admin user)

---
+++ tests/test_admin.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function assert() {
+    if [[ "$1" != "$2" ]]; then
+      echo "Test failed: expected '$2', but got '$1'"
+      exit 1
+    fi
+}
+
+echo "====================================================================================="
+echo "When an admin user is created, it should have access to all resources in the cluster"
+echo "======================================================================================"
+
+kubectl apply -f tests/admin_user.yaml
+assert "$(kubectl get user user1 -o jsonpath='{.metadata.name}')" "user1"
+assert "$(kubectl get user user1 -o jsonpath='{.spec.enabled}')" "true"
+
+kubectl get kubeconfig user1 -o json | jq .spec > kubeconfig
+kubectl get all --kubeconfig kubeconfig --namespace default
+kubectl get all --kubeconfig kubeconfig --namespace kube-system
+kubectl get crd --kubeconfig kubeconfig
+
+rm kubeconfig
+kubectl delete -f tests/admin_user.yaml
+echo "======================================================================================"
